9a857bc4eec142c504bbbdf036c6f92c9c9fc239
Binil Thomas Zachariah <binilreni7@gmail.com>
2023-07-28 13:09:24 -0400

    Lab completed

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_nos=list();
+    for n in num_list:
+        if n%2 ==0:
+            even_nos.append(n);
+    return even_nos;
+    
 
 def make_exclamation(sentence_list):
-    pass
+    new_sentance_list=list();
+    i=0
+    if(len(sentence_list)==0):
+        return new_sentance_list;
+    for n in sentence_list:
+        n=n+"!"
+        sentence_list[i]=n;
+        i=i+1;
+    return sentence_list;
+
